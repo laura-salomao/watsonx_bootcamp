2c2d0ae4a3db0182d45caf77d1623997606100e3
Laura Salomao Oliveira <90733076+laura-salomao@users.noreply.github.com>
2025-06-03 18:24:17 -0300

    Update models.py

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String, DateTime #importa tipos de dados e colunas do SQLAlchemy para definir a estrutura da tabela no banco de dados
 from sqlalchemy.orm import declarative_base #importa a função que cria a base para os modelos ORM (que mapeiam classes Python para tabelas SQL)
-from datetime import datetime #importa a data/hora atual do sistema, usada para registrar a data do pedido
+from datetime import datetime #importa a data atual do sistema, usada para registrar a data do pedido
 from pydantic import BaseModel, EmailStr #importa a classe BaseModel do Pydantic, usada para validação de dados da API / EmailStr garante que o email recebido tem formato válido
 
 Base = declarative_base() #cria a classe base usada para definir modelos SQLAlchemy. Todas as tabelas herdarão dela.
@@ -11,13 +11,13 @@ class Order(Base): #cria a classe Order, que representa uma tabela chamada order
     client_name = Column(String, nullable=False) #nome do cliente / nullable=False significa que é obrigatório
     client_email = Column(String, nullable=False) #email do cliente também é obrigatório
     order_number = Column(String, unique=True, nullable=False) #número do pedido / unique=True: o banco não permite dois pedidos com o mesmo número
-    order_date = Column(String, nullable=False) #data do pedido / Nao esta em formato "DateTime" para conseguir comparar com a data atual no watsonx assistant
+    order_date = Column(Date, nullable=False) #data do pedido / Nao esta em formato "DateTime" para conseguir comparar com a data atual no watsonx assistant
 
 class OrderCreate(BaseModel): #esse modelo é usado pelo FastAPI para validar dados quando o cliente envia um novo pedido
     client_name: str
     client_email: EmailStr
     order_number: str
-    order_date: str
+    order_date: date
 
 class OrderResponse(OrderCreate):
     id: int
